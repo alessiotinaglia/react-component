43ba0af2d14d8482c470fb3aeccc23922fc71531
alessiotinaglia <alessio.tina1998@gmail.com>
2024-12-12 17:20:05 +0100

    fine es completo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,12 @@ function card({cityImages}) {
     return (
         cityImages.map((element) =>
             <div key={element.id} className="card">
-                <img className="card-title" src={element.img} alt="Card image" />
+                <img className="card-foto" src={element.img} alt="Card image" />
                 <div className="card-body">
-                    <h5 className="card-title">{element.title}</h5>
-                    <p className="card-text">{element.description}</p>
+                    <h5>{element.title}</h5>
+                    <p>{element.description}</p>
                     <a href="#" className="btn btn-primary m-2">Prenota ora</a>
-                    <a href="#" className="btn btn-primary">Vedi offerte per te</a>
+                    <a href="#" className="btn btn-success ">Vedi offerte per te</a>
                 </div>
             </div>
         )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 function header() {
     return(
-        <header><h1>Il mio Blog</h1></header>
+        <header><h1>IL MIO BLOG</h1></header>
     )
 }
 
